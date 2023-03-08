5421b305df06715a87b33e02120550bef84b6055
unknown <alamgir6934@gmail.com>
2023-03-08 12:13:56 +0600

    appointment-page-added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => {
 
     const menuItems = <React.Fragment>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/'>Appointment</Link></li>
+        <li><Link to='/appointment'>Appointment</Link></li>
         <li><Link to='/'>About</Link></li>
-        <li><Link to='/'>Login</Link></li>
+        <li><Link to='/login'>Login</Link></li>
 
 
     </React.Fragment>
